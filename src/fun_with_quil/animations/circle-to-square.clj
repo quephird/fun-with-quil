455f528cc0a615a6d7c0e0e3ed38e9fcdcc83c9d
danielle kefford <danielle_kefford@hotmail.com>
2014-12-28 14:41:20 -0500

    Added another sketch.

---
+++ src/fun_with_quil/animations/circle-to-square.clj
@@ -0,0 +1,52 @@
+(ns fun-with-quil.animations.circle-to-square
+  (:require [quil.core :as q :include-macros true]
+            [quil.middleware :as m]))
+
+(defn make-cells [rows cols]
+  (into []
+    (for [_ (range cols)]
+      (into []
+        (for [_ (range rows)]
+          {:dθ (q/random 180)})))))
+
+(defn setup []
+  (let [rows 12 cols 18]
+    (q/stroke-weight 4)
+    (q/ellipse-mode :center)
+    (q/rect-mode :center)
+    (q/color-mode :hsb)
+    (q/smooth)
+    (make-cells rows cols)))
+
+(defn draw [cells]
+  (let [fc   (q/frame-count)
+        w    (q/width)
+        h    (q/height)
+        cell-h (/ h (count cells))
+        cell-w (/ w (count (first cells)))
+        inner-r (* 0.33 cell-w)
+        ]
+    (q/background 255)
+    (q/translate (* 0.5 cell-w) (* 0.5 cell-h))
+    (doseq [row cells]
+      (q/push-matrix)
+      (doseq [{dθ :dθ} row]
+        (let [outer-r (q/map-range (q/sin (* 4 (q/radians (+ fc dθ)))) -1 1 0 (* 0.5 cell-w))
+              b       (q/map-range (q/sin (* 4 (q/radians (+ fc dθ)))) -1 1 0 255)]
+          (q/fill 255)
+          (q/rect 0 0 (* 0.9 cell-w) (* 0.9 cell-h) outer-r)
+          (q/fill 255 0 b)
+          (q/ellipse 0 0 inner-r inner-r)
+          (q/translate 0 cell-w)))
+      (q/pop-matrix)
+      (q/translate cell-h 0))
+    )
+  )
+
+(q/defsketch circle-to-square
+  :title      "circle to square... and back again"
+  :size       [1440 800]
+  :setup      setup
+  :draw       draw
+  :middleware [m/fun-mode]
+  )
