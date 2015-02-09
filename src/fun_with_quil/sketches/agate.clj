985f9e586336a2cf868462a1edcefe7b63c472c7
danielle kefford <danielle_kefford@hotmail.com>
2015-02-09 00:28:17 -0500

    Some refactoring of agate sketch.

--- src/fun_with_quil/sketches/agate.clj
+++ src/fun_with_quil/sketches/agate.clj
@@ -2,67 +2,58 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
-(defn make-points []
-  (let [n (int (q/random 8 10))]
-    (for [i (range n)]
-      [(+ (q/random -50 50) (* 400 (q/cos (* i (/ q/TWO-PI n)))))
-       (+ (q/random -50 50) (* 300 (q/sin (* i (/ q/TWO-PI n)))))])))
-
 (defn setup []
   (q/smooth)
   (q/no-loop)
   (q/no-fill)
-  (q/color-mode :hsb)
-  (make-points)
-  )
+  (q/color-mode :hsb))
 
-(defn draw [points]
-  (let [fc (q/frame-count)
-        w  (q/width)
-        h  (q/height)
-        n  100
-        bs  (into [] (repeatedly n (fn [] (q/random 255))))
-        points' (concat (rest points) [(first points)])
-        pairs   (map vector points points')]
-    (q/background 0)
-    (q/translate (* 0.5 w) (* 0.5 h))
-    (q/rotate (q/radians (q/random -20 20)))
+; Computing arc centers algebraically is prohibitively difficult
+; so I decided to resort to a trigonometric algorithm.
+;
+; The strategy is: given a set of points somewhat randomly
+; generated around an ellipse, we first pair adjacent ones up.
+; Then for each pair, we need to compute the point from which
+; to generate concentric arcs by fixing a distance between
+; it and each of the points in the pair. From there, we need
+; to compute the angular width subtended by the pair onto
+; the arc center. We also need to determine the angle of rotation
+; between the line joining the arc center and the mindpoint to
+; orient the arcs properly.
+;
+;
+;     first point
+;       (x1,y1)
+;          *----__
+;                 `---__
+;                       `--_
+;                           `--_
+;                               `_
+;               (xm,ym)           \
+;               midpoint *          \
+;                                    \
+;                                     \
+;        (xc,yc) *                    |
+;       arc center                    |
+;                                     |
+;                                     *
+;                                 (x2,y2)
+;                               second point
+
+(defn draw-agate [x y]
+  (let [point-count (int (q/random 8 10))
+        n           60
+        points      (for [i (range point-count)]
+                      [(+ (q/random -50 50) (* 400 (q/cos (* i (/ q/TWO-PI point-count)))))
+                       (+ (q/random -50 50) (* 300 (q/sin (* i (/ q/TWO-PI point-count)))))])
+        bs          (into [] (repeatedly n (fn [] (q/random 255))))
+        points'     (concat (rest points) [(first points)])
+        pairs       (map vector points points')
+        agate-θ     (q/random -20 20)]
+    (q/push-matrix)
+    (q/translate x y)
+    (q/rotate agate-θ)
     (q/stroke-weight 6)
-    (q/stroke 0 255 0)
-
-    ; Computing arc centers algebraically is prohibitively difficult
-    ; so I decided to resort to a trigonometric algorithm.
-    ;
-    ; The strategy is: given a set of points somewhat randomly
-    ; generated around an ellipse, we first pair adjacent ones up.
-    ; Then for each pair, we need to compute the point from which
-    ; to generate concentric arcs by fixing a distance between
-    ; it and each of the points in the pair. From there, we need
-    ; to compute the angular width subtended by the pair onto
-    ; the arc center. We also need to determine the angle of rotation
-    ; between the line joining the arc center and the mindpoint to
-    ; orient the arcs properly.
-    ;
-    ;
-    ;     first point
-    ;       (x1,y1)
-    ;          *----__
-    ;                 `---__
-    ;                       `--_
-    ;                           `--_
-    ;                               `_
-    ;               (xm,ym)           \
-    ;               midpoint *          \
-    ;                                    \
-    ;                                     \
-    ;        (xc,yc) *                    |
-    ;       arc center                    |
-    ;                                     |
-    ;                                     *
-    ;                                 (x2,y2)
-    ;                               second point
-    ;
-    ;
 
     (doseq [i (range (dec n) 0 -1)]
       ; These computations need to be moved out of here
@@ -87,11 +78,17 @@
           (q/push-matrix)
           (q/translate xc yc)
           (q/rotate θ')
-          (q/stroke 10 255 (bs i))
+          (q/stroke 30 255 (bs i))
           (q/arc 0 0 d' d' (- ϕ) ϕ)
-          (q/pop-matrix)
-          ))))
-    (q/save "agate.png"))
+          (q/pop-matrix))))
+      (q/pop-matrix)))
+
+(defn draw [points]
+  (q/background 0)
+  (draw-agate 100 400)
+  (draw-agate 1500 0)
+  (draw-agate 1300 1400)
+  (q/save "agate.png"))
 
 (q/defsketch agate
   :title "agate"
