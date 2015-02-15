f243afa1a6b178efafc68d85f57200fc5ee12259
danielle kefford <danielle_kefford@hotmail.com>
2015-02-14 23:06:28 -0500

    Improved "o" sketch a little bit.

--- src/fun_with_quil/animations/o.clj
+++ src/fun_with_quil/animations/o.clj
@@ -3,13 +3,14 @@
 
 (defn setup []
   (q/no-stroke)
+  (q/color-mode :hsb)
   )
 
 (defn draw []
   (let [fc (q/frame-count)
         w  (q/width)
         h  (q/height)
-        n  12]
+        n  15]
     (q/background 0)
     (q/translate (* 0.5 w) (* 0.5 h))
 
@@ -18,27 +19,26 @@
             θ  (q/map-range (q/sin (+ dθ (* 0.05 fc))) -1 1 (q/radians -70) (q/radians 70))
             s  (+ 50 (* 30 i))
             c  (q/map-range i 0 (dec n) 255 0)
-            ]
+            ds (q/sqrt (* 2500 i))]
         (q/push-matrix)
         (q/rotate θ)
         (q/fill c)
         (q/begin-shape)
+
         (q/vertex s 0)
-        (q/vertex (* 0.5 s) (* -0.3 s))
-        (q/bezier-vertex (* 0.3 s) (* -0.6 s)
-                         (* -0.3 s) (* -0.6 s)
-                         (* -0.5 s) (* -0.3 s))
-        (q/vertex (* -0.5 s) (* -0.3 s))
+        (q/vertex (- s 20) -15)
+        (q/bezier-vertex (- s ds) (* -1.0 s)
+                         (- ds s) (* -1.0 s)
+                         (- 20 s) -15)
+        (q/vertex (- 20 s) -15)
+
         (q/vertex (- s) 0)
-        (q/vertex (* -0.5 s) (* 0.3 s))
-        (q/bezier-vertex (* -0.3 s) (* 0.6 s)
-                         (* 0.3 s) (* 0.6 s)
-                         (* 0.5 s) (* 0.3 s))
+        (q/vertex (- 20 s) 15)
+        (q/bezier-vertex (- ds s) (* 1.0 s)
+                         (- s ds) (* 1.0 s)
+                         (- s 20) 15)
         (q/end-shape :close)
-                (q/pop-matrix)
-))
-    )
-  )
+        (q/pop-matrix)))))
 
 (q/defsketch o
   :title "o"
