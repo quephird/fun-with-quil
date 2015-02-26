754efe4b9cb178c74765350033b4f7534794d1dc
danielle kefford <danielle_kefford@hotmail.com>
2015-02-26 12:19:31 -0500

    Renamed dandelion sketch, added color and movement.

--- src/fun_with_quil/animations/disco-dandelion.clj
+++ src/fun_with_quil/animations/disco-dandelion.clj
@@ -1,23 +1,22 @@
-(ns fun-with-quil.sketches.dandelion
+(ns fun-with-quil.sketches.disco-dandelion
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
 (defn make-dandelion [r]
-    (for [ϕ (range 0 181 30) θ (range 0 360 (/ 360 (+ 1 (* 11 (q/sin (q/radians ϕ))))))]
-        (let [x (+ (* r (q/cos (q/radians θ)) (q/sin (q/radians ϕ))) (q/random -50 50))
-              y (+ (* r (q/sin (q/radians θ)) (q/sin (q/radians ϕ))) (q/random -50 50))
-              z (+ (* r (q/cos (q/radians ϕ))) (q/random -10 10))]
-          {:x x :y y :z z :dz 0 :θ θ :ϕ ϕ})))
+    (for [ϕ (range 0 181 30)
+          θ (range 0 360 (/ 360 (+ 1 (* 11 (q/sin (q/radians ϕ))))))]
+      {:dz 0 :θ θ :ϕ ϕ}))
 
 (defn setup []
-;  (q/no-loop)
+  (q/color-mode :hsb)
   (make-dandelion 800)
   )
 
 (defn update [dandelion]
   (let [fc (q/frame-count)]
     (->> dandelion
-      (map (fn [seed] (assoc-in seed [:dz] (q/map-range (q/sin (* 0.1 fc)) -1 1 10 60)))))))
+      (map (fn [seed-head] (assoc-in seed-head[:dz] (q/map-range (q/sin (* 0.1 fc)) -1 1 10 60))))
+      (map (fn [seed-head] (update-in seed-head [:ϕ] + (q/radians (q/random -45 45))))))))
 
 (defn draw [dandelion]
   (let [fc (q/frame-count)
@@ -32,27 +31,30 @@
               0 0 0
               0 0 -1)
 
-    (doseq [{:keys [x y z dz θ ϕ]} dandelion]
-          (q/stroke-weight 3)
-          (q/stroke 90)
+    (doseq [{:keys [dz θ ϕ]} dandelion]
+      (let [x (* r (q/cos (q/radians θ)) (q/sin (q/radians ϕ)))
+            y (* r (q/sin (q/radians θ)) (q/sin (q/radians ϕ)))
+            z (* r (q/cos (q/radians ϕ)))]
+          (q/stroke-weight 5)
+          (q/stroke 90 100)
           (q/line 0 0 0 x y z)
           (q/push-matrix)
           (q/rotate-z (q/radians θ))
           (q/rotate-y (q/radians ϕ))
           (q/translate 0 0 r)
           (q/stroke-weight 1)
-          (q/stroke 255)
+          (q/stroke (mod fc 255) 255 255)
           (doseq [φ (range 0 360 20)]
-            (q/line 0 0 0
-                    (* 250 (q/cos (q/radians φ))) (* 250 (q/sin (q/radians φ))) dz))
+            (q/line 0 0 0 (* 250 (q/cos (q/radians φ))) (* 250 (q/sin (q/radians φ))) dz))
           (q/pop-matrix)
+        )
       )
     )
   )
 
-(q/defsketch dandelion
+(q/defsketch disco-dandelion
   :size [800 800]
-  :title "dandelion"
+  :title "disco dandelion"
   :setup setup
   :update update
   :draw draw
